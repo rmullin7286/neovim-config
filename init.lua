398aedc797d4c232bcf59f5b21de20f57c643aed
Ryan Mullin <rmullin7286@gmail.com>
2024-06-19 10:01:36 -0700

    Fixed catppuccin

--- init.lua
+++ init.lua
@@ -4,15 +4,15 @@
 -- up in different files but I found I don't use enough to justify that
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-  print("downloading lazy.nvim")
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable",
-    lazypath
-  })
+    print("downloading lazy.nvim")
+    vim.fn.system({
+        "git",
+        "clone",
+        "--filter=blob:none",
+        "https://github.com/folke/lazy.nvim.git",
+        "--branch=stable",
+        lazypath
+    })
 end
 vim.opt.rtp:prepend(lazypath)
 require("lazy").setup("plugins")

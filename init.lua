b672623aa7d0ded5a46ddbf0fd910ea44bbfd606
Ryan Mullin <rmullin7286>
2024-06-26 21:12:18 -0700

    Enabled relative line numbers

--- init.lua
+++ init.lua
@@ -41,8 +41,8 @@ vim.opt.shiftwidth = 4
 vim.opt.expandtab = true
 
 -- enable line numbers.
--- NOTE TO SELF: I should look into relative line numbers
 vim.opt.number = true
+vim.opt.relativenumber = true
 
 -- set colorscheme
 vim.cmd.colorscheme("catppuccin")

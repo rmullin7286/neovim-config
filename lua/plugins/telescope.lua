24f684093094d6a005c29f4526253a5c8f109317
Ryan Mullin <rmullin7286>
2024-08-21 23:34:14 -0700

    Added text search with telescope

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -7,5 +7,6 @@ return {
     config = function()
         local builtin = require("telescope.builtin")
         vim.keymap.set("n", "<leader>ff", builtin.find_files, {})
+        vim.keymap.set("n", "<leader>ft", builtin.live_grep, {})
     end
 }

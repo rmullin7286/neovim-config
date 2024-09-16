91d4f05f52f5b1d794858bf48153f0a8c1825a81
Ryan Mullin <rmullin7286>
2024-09-16 16:01:12 -0700

    Added debugging support

--- lua/keybindings.lua
+++ lua/keybindings.lua
@@ -11,3 +11,12 @@ end)
 -- tabs at the top of the editor.
 vim.keymap.set("n", "<Tab>", ":bnext<cr>")
 vim.keymap.set("n", "<S-Tab>", ":bprev<cr>")
+
+
+-- Debugging keybindings
+vim.keymap.set('n', '<leader>db', function() require('dap').toggle_breakpoint() end)
+vim.keymap.set('n', '<F5>', function() require('dap').continue() end)
+vim.keymap.set('n', '<F6>', function() require('dap').step_over() end)
+vim.keymap.set('n', '<F7>', function() require('dap').step_into() end)
+vim.keymap.set('n', '<F8>', function() require('dap').step_out() end)
+vim.keymap.set('n', '<leader>dh', function() require('dap.ui.widgets').hover() end)

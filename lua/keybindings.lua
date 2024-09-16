bcef911b94c79bd4093e4e76eac0b8770ec1fc85
Ryan Mullin <rmullin7286>
2024-09-16 16:29:23 -0700

    Fixed neotest keybindings

--- lua/keybindings.lua
+++ lua/keybindings.lua
@@ -1,11 +1,7 @@
-local neotest = require('neotest')
-
 -- These are bindings for neotest. <leader>tt will run the target closest to
 -- the cursor, and <leader>ft will run all targets in the file.
-vim.keymap.set('n', '<leader>tt', neotest.run.run)
-vim.keymap.set('n', '<leader>tf', function()
-    neotest.run.run(vim.fn.expand("%"))
-end)
+vim.keymap.set('n', '<leader>tt', function() require('neotest').run.run() end)
+vim.keymap.set('n', '<leader>tf', function() require('neotest').run.run(vim.fn.expand('%')) end)
 
 -- These bindings cycle through buffers, which will be displayed as
 -- tabs at the top of the editor.

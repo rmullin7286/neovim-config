504d352d54a2bd80a25591a169f6b777d4c3a2a4
Ryan Mullin <rmullin7286>
2024-09-16 13:50:23 -0700

    Added autopair rule for rust raw strings

--- lua/plugins/nvim-autopairs.lua
+++ lua/plugins/nvim-autopairs.lua
@@ -2,5 +2,15 @@ return {
     "windwp/nvim-autopairs",
     event = "InsertEnter",
     lazy = true,
-    config = true
+    config = function()
+        local pairs = require('nvim-autopairs')
+        local Rule = require('nvim-autopairs.rule')
+
+        pairs.setup({})
+
+        pairs.add_rules {
+            -- Rule for raw string literals in Rust
+            Rule('r#"', '"#', 'rust')
+        }
+    end
 }

45330b6a77fcf078a319da8c4c46ac644d15dc90
Ryan Mullin <rmullin7286>
2024-10-20 11:25:55 -0700

    Fixed haskell filetype commands. Updated dependencies

--- after/ftplugin/haskell.lua
+++ after/ftplugin/haskell.lua
@@ -1,1 +1,13 @@
 require('haskell-ftplugin-shared')
+
+local ht = require('haskell-tools')
+local bufnr = vim.api.nvim_get_current_buf()
+local opts = { noremap = true, silent = true, buffer = bufnr, }
+vim.keymap.set('n', '<leader>cl', vim.lsp.codelens.run, opts)
+vim.keymap.set('n', '<leader>hs', ht.hoogle.hoogle_signature, opts)
+vim.keymap.set('n', '<leader>ea', ht.lsp.buf_eval_all, opts)
+vim.keymap.set('n', '<leader>rr', ht.repl.toggle, opts)
+vim.keymap.set('n', '<leader>rf', function()
+    ht.repl.toggle(vim.api.nvim_buf_get_name(0))
+end, opts)
+vim.keymap.set('n', '<leader>rq', ht.repl.quit, opts)

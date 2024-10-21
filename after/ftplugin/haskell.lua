5e3f817191acb55872e0d61845954a496035f680
Ryan Mullin <rmullin7286>
2024-10-21 14:45:39 -0700

    Removed clashing keybindings

--- after/ftplugin/haskell.lua
+++ after/ftplugin/haskell.lua
@@ -5,7 +5,8 @@ local bufnr = vim.api.nvim_get_current_buf()
 local opts = { noremap = true, silent = true, buffer = bufnr, }
 vim.keymap.set('n', '<leader>cl', vim.lsp.codelens.run, opts)
 vim.keymap.set('n', '<leader>hs', ht.hoogle.hoogle_signature, opts)
-vim.keymap.set('n', '<leader>ea', ht.lsp.buf_eval_all, opts)
+-- This one clashes with nvim-tree
+-- vim.keymap.set('n', '<leader>ea', ht.lsp.buf_eval_all, opts)
 vim.keymap.set('n', '<leader>rr', ht.repl.toggle, opts)
 vim.keymap.set('n', '<leader>rf', function()
     ht.repl.toggle(vim.api.nvim_buf_get_name(0))
